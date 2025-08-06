2785054a212e1897dd6cc839cc47416ce5771006
suhaas3 <suhaas3w@gmail.com>
2025-08-06 07:46:14 +0530

    Redux Toolkit installed and store is Provided

---
+++ jsp/src/Redux-Toolkit-Reducers/InstahyreSlice.js
@@ -0,0 +1,18 @@
+ import { createSlice } from '@reduxjs/toolkit'
+
+const globalState = {
+
+}
+
+export const counterSlice = createSlice({
+  name: 'instahyre_form',
+  initialState: globalState,
+  reducers: {
+    
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { } = counterSlice.actions
+
+export default counterSlice.reducer
